ef0fb38567ccce46dc19ea3438e72bf6eced7749
Blake Ingenthron <blakeingenthron@gmail.com>
2020-03-24 19:16:09 -0500

    Update useMedia.js

--- src/utils/useMedia.js
+++ src/utils/useMedia.js
@@ -3,7 +3,8 @@ import { window } from 'browser-monads';
 
 export default function useMedia(queries, values, defaultValue) {
   const match = () =>
-    values[queries.findIndex(q => matchMedia(q).matches) || defaultValue];
+    values[queries.findIndex(q => window.matchMedia(q).matches)] ||
+    defaultValue;
   const [value, set] = useState(match);
   useEffect(() => {
     const handler = () => set(match);
